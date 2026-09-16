46006911717025f9c110263ffab267fd4a878f4a
EMajesty <lassi.laitinen@protonmail.com>
2026-09-16 22:18:03 +0300

    Add support for five nerve buttons

--- firmware/nerve/src/main.c
+++ firmware/nerve/src/main.c
@@ -14,8 +14,6 @@ static void enter_bootloader(void) {
     NVIC_SystemReset();
 }
 
-static struct gpio_callback button_cb;
-
 static void button_pressed(const struct device *port, struct gpio_callback *cb,
                            gpio_port_pins_t pins) {
     printk("button interrupt\n");
@@ -25,22 +23,37 @@ int main(void) {
     static const struct gpio_dt_spec led =
         GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
-    static const struct gpio_dt_spec button =
-        GPIO_DT_SPEC_GET(DT_ALIAS(nerve_button), gpios);
+    static const struct gpio_dt_spec buttons[] = {
+        GPIO_DT_SPEC_GET(DT_ALIAS(nerve_button1), gpios),
+        GPIO_DT_SPEC_GET(DT_ALIAS(nerve_button2), gpios),
+        GPIO_DT_SPEC_GET(DT_ALIAS(nerve_button3), gpios),
+        GPIO_DT_SPEC_GET(DT_ALIAS(nerve_button4), gpios),
+        GPIO_DT_SPEC_GET(DT_ALIAS(nerve_button5), gpios),
+    };
+
+    static struct gpio_callback button_cbs[ARRAY_SIZE(buttons)];
 
     static const struct device *console =
         DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
-    if (!gpio_is_ready_dt(&led) || !gpio_is_ready_dt(&button) ||
-        !device_is_ready(console)) {
+    if (!gpio_is_ready_dt(&led) || !device_is_ready(console)) {
         return 0;
     }
 
+    for (size_t i = 0; i < ARRAY_SIZE(buttons); i++) {
+        if (!gpio_is_ready_dt(&buttons[i])) {
+            return 0;
+        }
+    }
+
+    for (size_t i = 0; i < ARRAY_SIZE(buttons); i++) {
+        gpio_pin_configure_dt(&buttons[i], GPIO_INPUT);
+        gpio_init_callback(&button_cbs[i], button_pressed, BIT(buttons[i].pin));
+        gpio_add_callback_dt(&buttons[i], &button_cbs[i]);
+        gpio_pin_interrupt_configure_dt(&buttons[i], GPIO_INT_EDGE_TO_ACTIVE);
+    }
+
     gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure_dt(&button, GPIO_INPUT);
-    gpio_init_callback(&button_cb, button_pressed, BIT(button.pin));
-    gpio_add_callback_dt(&button, &button_cb);
-    gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
 
     printk("nerve online\n");
 
